8ee38eed464a7df442469f227b312c11f9e61d4d
ycd <ycd>
2023-04-22 01:38:15 +0800

    fix quickStart.sh

--- deployment/quickStart.sh
+++ deployment/quickStart.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "开始下载docker-compose.yaml"
-wget -s https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/docker-compose.yaml
-wget -s https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/IpProxyPool_Dockerfile
-wget -s https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/moviespider_Dockerfile
+wget -q https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/docker-compose.yaml
+wget -q https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/IpProxyPool_Dockerfile
+wget -q https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/moviespider_Dockerfile
 
 echo "创建目录"
 mkdir -p IpProxyPool movieSpider
 
 echo "下载配置文件"
-wget -s https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/IpProxyPool/config.yaml -O IpProxyPool/config.yaml
-wget -s https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/movieSpider/config.yaml -O movieSpider/config.yaml
+wget -q https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/IpProxyPool/config.yaml -O IpProxyPool/config.yaml
+wget -q https://raw.githubusercontent.com/YouCD/movieSpider/main/deployment/movieSpider/config.yaml -O movieSpider/config.yaml
 
 
 echo "启动 movieSpider"
